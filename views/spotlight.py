2569f0915f5d5877ab44d85d4b19baf84b5b6879
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2025-01-12 14:57:18 +0100

    Backstories working

--- views/spotlight.py
+++ views/spotlight.py
@@ -3,32 +3,24 @@ import streamlit as st
 from utils import load_csv
 
 
+# TODO: As of now, the backstories are generated everyday in Atenea, and these are send to Hiperion.
+#   The project generating the backstories is located at "/home/mrt/Projects/pix2pix". The project also contains a users.yaml file with user data to generate the backstories.
 def spotlight(last_file):
     st.title("Spotlight 🎇")
-    
+
     # Check if user moved to other view
-    if st.session_state.state != 'Spotlight':
-        st.session_state.state = 'Spotlight'
+    if st.session_state.state != "Spotlight":
+        st.session_state.state = "Spotlight"
 
     # Sort by debt
     debts_data = load_csv(last_file)
     sorted_debts = debts_data.sort_values(by="Debt", ascending=False).reset_index(drop=True)
     debtor = sorted_debts.loc[0, "Name"]
 
-    # Add an image
-    st.image("inputs/spotlight.png", caption=debtor, use_container_width=True)
+    # Display generated image
+    st.image(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/image.png", caption=debtor, use_container_width=True)
 
-    # Add a long text
-    prompt = """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor, 
-    nunc ac tempor luctus, lectus erat tristique eros, non dignissim libero arcu non ipsum. 
-    Cras id enim ut sapien scelerisque luctus. Ut efficitur tincidunt nisl, 
-    a posuere sapien congue sed. Sed aliquet purus nec varius ultricies. 
-    Nulla vehicula placerat metus sit amet egestas. Aenean vel sapien eget eros 
-    blandit fermentum. Nam luctus ultricies urna nec dictum.
-
-    In vel eros in felis malesuada tincidunt. Suspendisse feugiat turpis id nisi 
-    scelerisque, at gravida ex laoreet. Fusce vehicula faucibus eros ut consectetur. 
-    Aenean sit amet ex sed magna convallis congue ut ut quam.
-    """
+    # Display backstory
+    with open(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/backstory.txt", "r") as f:
+        prompt = f.read()
     st.markdown(prompt)
