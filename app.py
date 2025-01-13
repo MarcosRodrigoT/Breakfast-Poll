b3f2b1669fa1d5e26f2df6ae42e1ed524bcfe4f0
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2025-01-13 15:30:16 +0100

    Display top 3 morosos with nicknames

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ from utils import load_users, load_settleup, save_csv
 
 
 # Inputs directory
-USERS_FILE = "inputs/users.yaml"                 # Users, initial debts, and description
+USERS_FILE = "inputs/users.yaml"  # Users, initial debts, and description
 
 # History directory
 HISTORY_DIR = "history"
 os.makedirs(HISTORY_DIR, exist_ok=True)
-LST_FILE = os.path.join(HISTORY_DIR, "last.csv") # Last debts
+LST_FILE = os.path.join(HISTORY_DIR, "last.csv")  # Last debts
 
 # Tmp directory
 TMP_DIR = "tmp"
 os.makedirs(TMP_DIR, exist_ok=True)
 WHO_FILE = os.path.join(TMP_DIR, "whopaid.txt")  # Who paid
-ORD_FILE = os.path.join(TMP_DIR, "order.csv")    # The order
-BAR_FILE = os.path.join(TMP_DIR, "bar.csv")      # What to ask at the bar
+ORD_FILE = os.path.join(TMP_DIR, "order.csv")  # The order
+BAR_FILE = os.path.join(TMP_DIR, "bar.csv")  # What to ask at the bar
 MAC_FILE = os.path.join(TMP_DIR, "machine.csv")  # What to put in the paying machine
-DEB_FILE = os.path.join(TMP_DIR, "debts.csv")    # Debts per user
+DEB_FILE = os.path.join(TMP_DIR, "debts.csv")  # Debts per user
 
 # Set name and icon to webpage
 st.set_page_config(
@@ -30,35 +30,34 @@ st.set_page_config(
 )
 
 # Initialize session state
-if 'state' not in st.session_state:
-    st.session_state.state = 'Poll'
+if "state" not in st.session_state:
+    st.session_state.state = "Poll"
 if "users" not in st.session_state:
     st.session_state.users = load_users(USERS_FILE)
 if not os.path.isfile(LST_FILE):
     save_csv(load_settleup(USERS_FILE), LST_FILE)
 
 # Sidebar for navigating through different views
-menu = st.sidebar.selectbox("Select View", ["Poll ☕", "Current 💥", "Debts 💲", "History 📜", "Spotlight 🎇"])
+menu = st.sidebar.selectbox("Select View", ["Poll ☕", "Current 💥", "Debts 💲", "History 📜", "Morosos 👻"])
 match menu:
-    
     # Poll view to create an order
     case "Poll ☕":
         poll(ORD_FILE)
-    
+
     # Current view to display the current order
     case "Current 💥":
         current(HISTORY_DIR, WHO_FILE, ORD_FILE, BAR_FILE, MAC_FILE, DEB_FILE, LST_FILE)
-    
+
     # Debts view to check debts
     case "Debts 💲":
         debts(USERS_FILE, LST_FILE)
-    
+
     # History view to check past summaries
     case "History 📜":
         history(HISTORY_DIR, WHO_FILE, ORD_FILE, BAR_FILE, MAC_FILE, DEB_FILE)
-    
+
     # Spotlight view to see the story of the person with larger debt
-    case "Spotlight 🎇":
+    case "Morosos 👻":
         spotlight(LST_FILE)
 
 
--- views/spotlight.py
+++ views/spotlight.py
@@ -6,7 +6,7 @@ from utils import load_csv
 # TODO: As of now, the backstories are generated everyday in Atenea, and these are send to Hiperion.
 #   The project generating the backstories is located at "/home/mrt/Projects/pix2pix". The project also contains a users.yaml file with user data to generate the backstories.
 def spotlight(last_file):
-    st.title("Spotlight 🎇")
+    st.title("Morosos 👻")
 
     # Check if user moved to other view
     if st.session_state.state != "Spotlight":
@@ -15,12 +15,22 @@ def spotlight(last_file):
     # Sort by debt
     debts_data = load_csv(last_file)
     sorted_debts = debts_data.sort_values(by="Debt", ascending=False).reset_index(drop=True)
-    debtor = sorted_debts.loc[0, "Name"]
 
-    # Display generated image
-    st.image(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/image.png", caption=debtor, use_container_width=True)
+    # Display generated images and backstories for top 3 debtors
+    for i, symbol in zip(range(3), ["🥇", "🥈", "🥉"]):
+        debtor = sorted_debts.loc[i, "Name"]
 
-    # Display backstory
-    with open(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/backstory.txt", "r") as f:
-        prompt = f.read()
-    st.markdown(prompt)
+        # Obtain debtor's nickname
+        with open(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/nickname.txt", "r") as f:
+            nickname = f.read()
+
+        # Display debtor name as subtitle
+        st.subheader(f"{symbol}{debtor}, {nickname}")
+
+        # Display generated image
+        st.image(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/image.png", use_container_width=True)
+
+        # Display backstory
+        with open(f"/home/mrt/Projects/pix2pix/backstories/{debtor}/backstory.txt", "r") as f:
+            prompt = f.read()
+        st.markdown(prompt)
